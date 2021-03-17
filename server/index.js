70415fe6c363d35876bf042c151cb168a3cecd0e
Сергей Суслов <suslovsd@lad24.ru>
2021-03-17 22:41:48 +0300

    добавил серверную часть

--- server/index.js
+++ server/index.js
@@ -1,54 +1,46 @@
-const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const expressPlayground = require("graphql-playground-middleware-express")
-  .default;
-const { readFileSync } = require("fs");
-const typeDefs = readFileSync("./typeDefs.graphql", "UTF-8");
-const resolvers = require("./resolvers");
-const { MongoClient } = require("mongodb");
-require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema");
 
-const {
-  MONGO_USERNAME,
-  MONGO_PASSWORD,
-  MONGO_HOSTNAME,
-  MONGO_PORT,
-  MONGO_DB,
-} = process.env;
+const users = [
+  {
+    id: 1,
+    username: "admin",
+    age: 28,
+  },
+];
 
-async function start() {
-  var app = express();
+const createUser = (input) => {
+  const id = Date.now();
+  return { id, ...input };
+};
 
-  const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-  console.log("url :>> ", url);
-  const client = await MongoClient.connect(url, { useNewUrlParser: true });
+const app = express();
 
-  const db = client.db();
+app.use(cors());
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: async ({ req }) => {
-      const githubToken = req.headers.authorization;
-      console.log("githubToken :>> ", githubToken);
-      const currentUser = await db.collection("users").findOne({ githubToken });
-      return { db, currentUser };
-    },
-  });
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+  getUser: ({ id }) => {
+    return users.find((user) => user.id == id);
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    users.push(user);
+    return user;
+  },
+};
 
-  server.applyMiddleware({ app });
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
+  })
+);
 
-  app.get("/", (req, res) => {
-    let url = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user`;
-    res.end(`<a href="${url}">Sign In with Github</a>`);
-  });
-  app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
-  app.listen({ port: 4000 }, () =>
-    console.log(
-      `GraphQL Server running at
-    http://localhost:4000${server.graphqlPath}`
-    )
-  );
-}
-
-start();
+app.listen(5000, () => console.log("сервер запустился"));
