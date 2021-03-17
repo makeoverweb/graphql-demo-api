8e20eb6b338c8d5db682279dc6ec1dcc74eb0b83
Сергей Суслов <suslovsd@lad24.ru>
2021-03-17 22:43:10 +0300

    new commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,3 @@
-import Users from "./Users";
-import { gql } from "apollo-boost";
-import AuthorizedUser from "./AuthorizedUser";
-import { BrowserRouter } from "react-router-dom";
-
-export const ROOT_QUERY = gql`
-  query allUsers {
-    totalUsers
-    allUsers {
-      ...userInfo
-    }
-    me {
-      ...userInfo
-    }
-  }
-  fragment userInfo on User {
-    githubLogin
-    name
-    avatar
-  }
-`;
-
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <AuthorizedUser />
-      <Users />
-    </div>
-  </BrowserRouter>
-);
+const App = () => <div>234</div>;
 
 export default App;
